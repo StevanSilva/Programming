7e0e098558ddd3dd2daceec70ad180f61fa050a2
Stevan Bernardino Silva <87912386+StevanSilva@users.noreply.github.com>
2021-08-16 19:17:02 -0300

    Update imc.js

--- imc.js
+++ imc.js
@@ -1,93 +1,63 @@
-//Variáveis-----------------------------------------------
+let niveis = ["muito abaixo do peso",
+              "abaixo do peso",
+              "peso normal",
+              "acima do Peso",
+              "obesidade grau I",
+              "obesidade grau II",
+              "obesidade grau III"]
 
-let niveis = ["Muito abaixo do peso",
-              "Abaixo do peso",
-              "Peso normal",
-              "Acima do peso",
-              "Obesidade grau I",
-              "Obesidade grau II",
-              "Obesidade grau III"]
-
-
-let pessoas = ["Stevan", "Pedro", "Eduardo"]
-let massas = [76, 72, 89]
-let alturas = [1.82, 1.85, 1.80]
+let pessoas = ["Stevan Bernardino", "Pedro Naves", "Eduardo Stuart"]
+let massas = [76, 70, 89]
+let alturas = [1.82, 1.80, 1.80]
 let idades = [33, 28, 34]
+let sexos = ["masculino","masculino", "masculino" ]
 
-//--------------------------------------------------------
-
-//Funções-------------------------------------------------
-
-function calcularIMC (massa, altura){
-let IMC = massa/(altura**2)
-return IMC
+function calcularIMC(massa, altura){
+  let IMC = massa/(altura**2)
+  return IMC
 }
 
-function verificaNivel (IMC){
+function verificarNivel(IMC){
   let nivel = 0
 
-  if (IMC < 17){
-  let nivel = 0
-}else if (IMC < 18.5){
+if (IMC < 17){
+  nivel = 0
+}else if(IMC < 18.5){
   nivel = 1
-}else if (IMC < 25){
+}else if(IMC < 25){
   nivel = 2
-}else if (IMC < 30){
+}else if(IMC < 30){
   nivel = 3
-}else if (IMC < 35){
+}else if(IMC < 35){
   nivel = 4
-}else if (IMC < 40){
+}else if(IMC < 40){
   nivel = 5
 }else{
   nivel = 6
 }
-
 return nivel
-
 }
 
-//--------------------------------------------------------
-
-//Execução do código--------------------------------------
-
-
-
-let indice = 0
-let pessoa = pessoas[indice]
-let massa = massas[indice]
-let altura = alturas[indice]
-let idade = idades[indice]
-
-let IMC = calcularIMC(massa, altura)
-let nivel = verificaNivel(IMC)
-
-console.log("Nome: " + pessoa)
-console.log("Idade: " + idade)
-console.log("Peso: " + massa)
-console.log("Altura: " + altura)
-console.log("IMC: " + IMC.toFixed(2)
-console.log("Nível: " + niveis[nivel])
-
-/*for (let indice = 0; indice < pessoas.length; indice++){
+for (let indice = 0; indice < pessoas.length; indice++){
 
 let pessoa = pessoas[indice]
 let massa = massas[indice]
 let altura = alturas[indice]
 let idade = idades[indice]
+let sexo = sexos[indice]
 
 let IMC = calcularIMC(massa, altura)
-let nivel = definirNivelDoIMC(IMC)
+let nivel = verificarNivel(IMC)
 
 console.log("Nome: " + pessoa)
 console.log("Peso: " + massa)
 console.log("Altura: " + altura)
 console.log("Idade: " + idade)
 console.log("IMC: " + IMC.toFixed(2))
-console.log("Nível do IMC: " + niveis[nivel])
+console.log("Estado: " + niveis[nivel])
+console.log("Sexo: " + sexo)
 console.log("")
+
 }
-*/
 
-// ou calculo desse modo para varrer todos os nomes. 
 
-//--------------------------------------------------------
