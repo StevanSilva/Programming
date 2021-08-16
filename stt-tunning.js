da68ff787ef7c5236e5ac1e075f229312c873871
Stevan Bernardino Silva <87912386+StevanSilva@users.noreply.github.com>
2021-08-16 19:11:36 -0300

    Add files via upload

---
+++ stt-tunning.js
@@ -0,0 +1,18 @@
+let stt = 0
+
+while (stt < 10){
+  stt = stt + 1
+  console.log("Stt: " + stt)
+} 
+
+let tunning = 0
+while (tunning < 20){
+  tunning = tunning + 1
+  console.log("Tunning: " + tunning)
+}
+
+let developer = 0
+while (developer < 20){
+  developer = developer + 1
+  console.log("Developer: " + developer)
+}
