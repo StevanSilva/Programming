bcba03082298aaba6056268b482353c492433f45
Stevan Bernardino Silva <87912386+StevanSilva@users.noreply.github.com>
2021-08-17 15:23:53 -0300

    Update divisíveis2.js

--- divisíveis2.js
+++ divisíveis2.js
@@ -5,3 +5,13 @@ while (num <= 100){
 	console.log(num)
 	num = num + 2
 } 
+
+// ou
+
+for (let i=0; i <=100; i++){
+if ((i % 2) === 0)
+console.log(i)
+}
+
+// % verifica o resto, e não o resultado 
+// Sobre o if (x % 2 == 0). O operador % é chamado de módulo. Ele serve para você verificar qual o resto da divisão de um número por outro... No seu caso, você está verificando se o resto da divisão de x por 2 resulta em 0, ou seja, você está verificando se x é múltiplo de 2.
