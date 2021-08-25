9cf8df4962c8f4c463d415444f256fd894798ee7
Stevan Bernardino Silva <87912386+StevanSilva@users.noreply.github.com>
2021-08-25 15:09:55 -0300

    Add files via upload

---
+++ ano.js
@@ -0,0 +1,19 @@
+/*
+Ler o ano atual e o ano de nascimento de uma pessoa. Escrever uma mensagem que diga se ela
+poderá ou não votar este ano (não é necessário considerar o mês em que a pessoa nasceu).
+*/
+
+let anoAtual = 2021
+let anoNascimento = 2004
+
+if (anoNascimento <= 2003)
+{
+  console.log("Pode votar")
+}
+
+else 
+{
+  console.log("Não pode votar")
+}
+
+
